7d72b1efd7df4ce4f83a15fa78b6ba41f849effd
Patrice Juergens <pj@validitylabs.org>
2020-04-30 12:57:38 +0200

    ✨ Add async container component example

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+// import axios from 'axios';
 import { Shell } from '../Shell';
 
-export const Root = () => {
-    return <Shell />;
-};
+interface IState {
+    data: string;
+}
+
+export class Root extends React.Component<any, IState> {
+    // state = {
+    //     data: '',
+    // };
+
+    // componentDidMount = () => {
+    //     this.getData();
+    // };
+
+    // getData = async () => {
+    //     const res = await axios.get('https://some-resource.validitylabs.org');
+    //     this.setState({ data: res.data });
+    // };
+
+    render() {
+        // if (this.state.data) {
+        //     console.log(this.state.data);
+        // }
+        return <Shell />;
+    }
+}
